29fbd870177531e51924708d97c0626cfc2c28f3
geoffrey woodcock <theatomproject@gmail.com>
2018-06-12 19:33:47 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,3 +1,3 @@
-CREATE TABLE bears (id INTEGER, name TEXT, age INTEGER,
+CREATE TABLE bears (id INTEGER PRIMARY KEY, name TEXT, age INTEGER,
         gender CHAR(1), color TEXT, temperament TEXT, alive BOOLEAN);
         
